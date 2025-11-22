7a20fc5be82d4080cbbd2175a6ad16fe18abd44f
quinticenshal <154838405+quinticenshal@users.noreply.github.com>
2025-11-22 12:04:48 -0400

    Delete Jeopardy/game directory

--- Jeopardy/game/src/main/java/com/jeopardy/Reader.java
+++
@@ -1,6 +0,0 @@
-package com.jeopardy;
-import java.util.ArrayList;
-
-public interface Reader{
-    public ArrayList<Question> read();
-}
--- Jeopardy/game/src/main/java/com/jeopardy/Writer.java
+++
@@ -1,7 +0,0 @@
-package com.jeopardy;
-
-import java.util.ArrayList;
-
-public interface Writer{
-    public void write(ArrayList<String> statements);
-}
