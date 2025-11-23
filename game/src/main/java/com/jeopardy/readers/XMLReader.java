b95b9959c75f31dfbf2e114f3eb6644cbc450611
II-117 <143234129+II-117@users.noreply.github.com>
2025-11-22 21:18:55 -0400

    fixed imports
    removed import java.beans.Statement; and added import java.io.File;

--- game/src/main/java/com/jeopardy/readers/XMLReader.java
+++ game/src/main/java/com/jeopardy/readers/XMLReader.java
@@ -4,70 +4,75 @@ import java.util.ArrayList;
 import org.w3c.dom.Element;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-
 import com.jeopardy.Question;
+import java.io.File;
 
-import java.beans.Statement;
-import java.io.IOException;
-
-public class XMLReader implements Reader{
+public class XMLReader implements Reader {
     private String file;
     private DocumentBuilderFactory factory;
     private DocumentBuilder builder;
     private Document document;
     private ArrayList<Question> questions = new ArrayList<Question>();
 
-    public XMLReader(String file){
-       this.file = file;  
+    public XMLReader(String file) {
+        this.file = file;
     }
-   
-   public ArrayList<Question> read(){
-            try {
-                factory = DocumentBuilderFactory.newInstance();
-                builder = factory.newDocumentBuilder();
-                document = builder.parse(new File(file));
-                document.getDocumentElement().normalize();
 
-                NodeList node = document.getElementsByTagName("QuestionItem");
+    public ArrayList<Question> read() {
+        try {
+            factory = DocumentBuilderFactory.newInstance();
+            builder = factory.newDocumentBuilder();
+            document = builder.parse(new File(file));
+            document.getDocumentElement().normalize();
 
-                for (int i = 0; i < nodes.getLength(); i++ ){
-                    Element e = (Element) nodes.item(i);
-                    Question q = new Question();
+            NodeList nodes = document.getElementsByTagName("QuestionItem");
 
-                    q.setCategory(getElementsByTagName(e, "Category"));
+            for (int i = 0; i < nodes.getLength(); i++) {
+                Element e = (Element) nodes.item(i);
+                Question q = new Question();
 
-                    String valueText = getElementsByTagName(e, "Value");
+                q.setCategory(getElementText(e, "Category"));
 
-                    try {
-                        q.setValue(Integer.parseInt(valueText));
-                    } catch (NumberFormatException ex) {
-                        System.err.println("Invalid value format for question " + i + ": " + valueText);
-                        q.setValue(0);
-                    }
-
-                    q.setStatement(getElementsByTagName(e, Statement));
-
-                    q.setA(getElementsByTagName(e, "OptionA"));
-                    q.setB(getElementsByTagName(e, "OptionB"));
-                    q.setC(getElementsByTagName(e, "OptionC"));
-                    q.setD(getElementsByTagName(e, "OptionD"));
-
-                    q.setCorrect(getElementsByTagName(e, "CorrectAnswer"));
-
-                    questions.add(q);
-
+                String valueText = getElementText(e, "Value");
+                try {
+                    q.setValue(Integer.parseInt(valueText));
+                } catch (NumberFormatException ex) {
+                    System.err.println("Invalid value format for question " + i + ": " + valueText);
+                    q.setValue(0);
                 }
 
-            } catch (Exception e) {
+                q.setStatement(getElementText(e, "QuestionText"));
+
+                // Get options
+                NodeList optionsList = e.getElementsByTagName("Options");
+                if (optionsList.getLength() > 0) {
+                    Element optionsElement = (Element) optionsList.item(0);
+                    q.setA(getElementText(optionsElement, "OptionA"));
+                    q.setB(getElementText(optionsElement, "OptionB"));
+                    q.setC(getElementText(optionsElement, "OptionC"));
+                    q.setD(getElementText(optionsElement, "OptionD"));
+                }
+
+                q.setCorrect(getElementText(e, "CorrectAnswer"));
+
+                questions.add(q);
+            }
+
+        } catch (Exception e) {
             System.err.println("Error reading XML file: " + e.getMessage());
             e.printStackTrace();
         }
-        
+
         return questions;
     }
-}
 
+    private String getElementText(Element parent, String tagName) {
+        NodeList nodeList = parent.getElementsByTagName(tagName);
+        if (nodeList.getLength() > 0) {
+            return nodeList.item(0).getTextContent();
+        }
+        return "";
+    }
+}
